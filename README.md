9fba255101075cfe8c53a10fb867745b4cc6c526
Mathieu Clement <mathieu.clement@fairtiq.com>
2022-10-21 22:26:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Swiss Post Packages
+
+## Description
+
+Currently this is just a little bit of code to download the list of packages from the [Swiss Post](https://www.post.ch)
+website and parse them to Python objects.
+
+Eventually I'd like to integrate this into a Home Assistant component. It could be fun to see if packages
+are expected on the current day or following days, and maybe merge data from multiple data, create automations with this information, etc.
+
+## Available Data
+
+At the moment the following information is available:
+
+  - Package ID
+  - Sender name
+  - Delivery status (in transit or delivered; more to be implemented as I discover them)
+  - Delivery date (expected or actually delivered)
+  - Tracking URL
+
+
+The tracking URL can be used to fetch additional information about parcels such as the size, 
+weight, whether they fit in the mailbox, and even a picture.
+
+## Implementation Details
+
+There are existing Home Assistant integrations for this kind of thing, but they rely on parsing your e-mails
+and I'm not a big fan of that from a security and technical perspective.
+
+Ideally I'd have used only the requests Python library and kept cookies in memory, but it proved
+a little too time consuming for this proof-of-concept so I went with Selenium, opening a headless browser
+(Firefox), logging in, and downloading the parcel data as JSON and then converting that to Python objects.
+
+The Selenium web driver (Firefox) is installed automatically.
+
+## Installation
+
+Create a virtual environment and install required packages:
+
+```bash
+python -m venv venv
+pip install -r requirements.txt
+```
+
+Eventually, depending on how this gets deployed, I'll make it either installable in HACS (Home Assistant
+Community Store) or through Docker.
+
+Create a `credentials.py` file with the following content:
+
+```python
+EMAIL = '<YOUR SWISS ID EMAIL ADDRESS>'
+PASSWORD = '<YOUR PASSWORD>'
+```
+
+Similarly here, depending on how this gets deployed, this is likely to change in the future.
+
+## Usage
+
+Run with:
+
+```bash
+. ./venv/bin/activate
+python app.py
+```
+
+of course right now it's at the very early stages, so just take a look at `app.py` if you want to use it
+in your project as-is.
+
+## License
+
+>Swiss Post Packages - Scrapes parcels from the post.ch website
+>Copyright (C) 2022 Mathieu Clément
+>
+>This program is free software: you can redistribute it and/or modify
+>it under the terms of the GNU General Public License as published by
+>the Free Software Foundation, either version 3 of the License, or
+>(at your option) any later version.
+>
+>This program is distributed in the hope that it will be useful,
+>but WITHOUT ANY WARRANTY; without even the implied warranty of
+>MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+>GNU General Public License for more details.
+
+See [https://www.gnu.org/licenses/](https://www.gnu.org/licenses/) for a copy of the GNU General Public License.
---
+++ app.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from datetime import datetime
+from enum import Enum, auto
+import json
+import logging
+import json
+import pprint as pp
+import time
+
+
+class PackageStatus(Enum):
+    IN_TRANSIT = auto()
+    DELIVERED = auto()
+    UNKNOWN = auto()
+
+
+class Package:
+    def __init__(self, identifier, sender, status, delivery_date, tracking_url):
+        self.identifier = identifier
+        self.sender = sender
+        self.status = status
+        self.delivery_date = delivery_date
+        self.tracking_url = tracking_url
+
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+# TODO: Use config file
+from credentials import EMAIL, PASSWORD
+
+class Scraper:
+    def __init__(self, headless=True, email=EMAIL, password=PASSWORD):
+        self.logger = logging.getLogger('SwissPostPackages.Scraper')
+        self.email = email
+        self.password = password
+
+        options = Options()
+        options.headless = headless
+        service = Service(executable_path=GeckoDriverManager().install())
+        self.driver = webdriver.Firefox(options=options, service=service)
+        self.driver.implicitly_wait(10)
+
+        self.logged_in = False
+
+    
+    def login(self):
+        if not self.logged_in:
+            self._login()
+
+
+    def _login(self):
+        self.logger.info('Logging in')
+
+        self.logger.debug('Loading "My Post" page')
+        login_url = 'https://www.post.ch/fr/espace-clients'
+        self.driver.get(login_url)
+
+        self.logger.debug('Clicking on button to log in with Swiss ID')
+        self.driver.find_element(By.ID, 'externalIDP').click()
+
+        self.logger.debug('Typing in e-mail address and password')
+
+        email_field = self.driver.find_element(By.ID, 'email')
+        email_field.clear()
+        email_field.send_keys(self.email)
+
+        password_field = self.driver.find_element(By.ID, 'password')
+        password_field.clear()
+        password_field.send_keys(self.password)
+
+        self.logger.debug('Triggering login')
+        password_field.send_keys(Keys.RETURN)
+
+        self.logger.debug('Waiting until logged in')
+        try:
+            self.wait_until_url_contains('https://service.post.ch/kvm/app/ui/')
+        except:
+            raise Error('Could not log in. Did you provide the correct credentials?')
+        time.sleep(2) # Browser seems overwhelmed if we move on too quickly
+        self.logger.debug('Logged in!')
+        self.logged_in = True
+
+
+    def fetch_packages_json(self):
+        self.logger.info('Fetching packages')
+        self.driver.get('view-source:https://service.post.ch/kvm/app/api/api/activities?page=0&limit=1000&lang=en')
+        json_string = self.driver.find_element(By.TAG_NAME, 'pre').text
+        result = json.loads(json_string)
+        return result
+
+
+    def wait_until_url_contains(self, partial_url):
+        self.logger.debug('Waiting until URL contains %s', partial_url)
+        return WebDriverWait(self.driver, 10).until(self.url_contains_function(partial_url))
+
+    
+    def url_contains_function(self, partial_url):
+        def condition(driver):
+            return partial_url in driver.current_url
+        return condition
+
+
+    def fetch_packages(self):
+        self.login()
+        packages = [self.to_package(data) for data in self.fetch_packages_json() if 'type' in data and data['type'] == 'PARCEL']
+        return packages
+
+
+    def to_package(self, data):
+        package = Package(data['id'], data['summary'], self.to_status(data['statusType']), 
+                          datetime.fromisoformat(data['date']), data['actions'][0]['url'])
+        return package
+
+
+    def to_status(self, status_type):
+        if status_type == 'ON_GOING_DELIVERY':
+            return PackageStatus.IN_TRANSIT
+        elif status_type == 'DELIVERED':
+            return PackageStatus.DELIVERED
+        else:
+            self.logger.warn('Unknown package status: %s' % status_type)
+            return PackageStatus.UNKNOWN
+
+
+class App:
+    def __init__(self):
+        self.logger = logging.getLogger('SwissPostPackages.App')
+        self.scraper = Scraper()
+        self.packages_list = None
+
+    @property
+    def packages(self):
+        if self.packages_list is None:
+            self.packages_list = self.scraper.fetch_packages()
+        return self.packages_list
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logger = logging.getLogger('SwissPostPackages')
+    logger.setLevel(logging.DEBUG)
+
+    app = App()
+    packages = app.packages
+    pp.pprint(packages)
